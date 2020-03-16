bb3611ae3edc04fdef0c856f06bb72705d89030f
Supto Purkayasto <52487890+developersuptop@users.noreply.github.com>
2020-03-17 01:29:28 +0600

    rename CanShowHomePageTest to HomePageTest

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CanShowHomePageTest extends TestCase
+class HomePageTest extends TestCase
 {
     /** @test  */
 
