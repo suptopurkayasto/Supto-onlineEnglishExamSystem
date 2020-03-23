b5fbb1b7d088f1afed93bd3f16a498b4d5e23659
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-24 00:21:48 +0600

    Create Question Category CRUD & Seeder files

---
+++ database/factories/QuestionCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\QuestionCategory;
+use Faker\Generator as Faker;
+
+$factory->define(QuestionCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
