e9562d3097573c085013e2e7175e6e6d5dda3192
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-24 16:31:08 +0600

    Create Question Set resource files

---
+++ app/Http/Controllers/Admin/Question/QuestionSetController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Question;
+
+use App\Http\Controllers\Controller;
+use App\QuestionSet;
+use Illuminate\Http\Request;
+
+class QuestionSetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\QuestionSet  $questionSet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QuestionSet $questionSet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\QuestionSet  $questionSet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QuestionSet $questionSet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\QuestionSet  $questionSet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, QuestionSet $questionSet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\QuestionSet  $questionSet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QuestionSet $questionSet)
+    {
+        //
+    }
+}
