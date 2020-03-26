8724ec23944b9f75872fc1d0fcc1ed902bfd962c
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-26 15:05:51 +0600

    Modify home page functionality

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest:student');
+    }
     public function index()
     {
         return view('pages.home');
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', 'HomePageController@index');
+Route::get('/', 'HomePageController@index');
 //
 //Auth::routes();
 //
