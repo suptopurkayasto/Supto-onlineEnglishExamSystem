5e900257a04145471d827d2eae8de7b62b396cd7
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-27 23:09:08 +0600

    Fix not found issue and modify student index file

--- app/Student.php
+++ app/Student.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use App\GrammarQuestion\StudentGrammarQuestionExamGotMarks;
+use App\Model\Grammar\StudentGrammarQuestionExamGotMarks;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
