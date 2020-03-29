ebf2e76c46e7b828550c162f3b5ea5ccde770566
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-29 15:31:42 +0600

    Create InformalEmailController.php

---
+++ app/Http/Controllers/Teacher/Question/Writing/Email/InformalEmailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Teacher\Question\Writing\Email;
+
+use App\Http\Controllers\Controller;
+use App\Model\Writing\InformalEmail;
+use Illuminate\Http\Request;
+
+class InformalEmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Writing\InformalEmail  $informalEmail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InformalEmail $informalEmail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Writing\InformalEmail  $informalEmail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InformalEmail $informalEmail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Writing\InformalEmail  $informalEmail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InformalEmail $informalEmail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Writing\InformalEmail  $informalEmail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InformalEmail $informalEmail)
+    {
+        //
+    }
+}
