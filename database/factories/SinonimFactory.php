7ef00af8fc093bc22e8b42a72849558a3763ccaf
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-03-30 16:59:08 +0600

    Create Sinonim Model, and seeder files

---
+++ database/factories/SinonimFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Vocabulary\Sinonim;
+use Faker\Generator as Faker;
+
+$factory->define(Sinonim::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
