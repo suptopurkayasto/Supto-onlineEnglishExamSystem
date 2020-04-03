78aaa92598240041d25d2afa79ceb2306f82cc59
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-04-03 15:33:00 +0600

    Create Definition Model, Controller, Factory, Seeder files

---
+++ app/Http/Controllers/Teacher/Question/Vocabulary/Definition/DefinitionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Teacher\Question\Vocabulary\Definition;
+
+use App\Http\Controllers\Controller;
+use App\Model\Vocabulary\Definition\Definition;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class DefinitionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Definition $definition
+     * @return Response
+     */
+    public function show(Definition $definition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Definition $definition
+     * @return void
+     */
+    public function edit(Definition $definition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Definition $definition
+     * @return Response
+     */
+    public function update(Request $request, Definition $definition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Definition $definition
+     * @return Response
+     */
+    public function destroy(Definition $definition)
+    {
+        //
+    }
+}
---
+++ app/Model/Vocabulary/Definition/Definition.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Vocabulary\Definition;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Definition extends Model
+{
+    protected $guarded = [];
+}
