ee6992e4a2976f3e8c14158f853ef7d6dee913b2
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-04-04 02:13:30 +0600

    Create FillInTheGap Model, Migration, Factory, Seeder Controller

---
+++ app/Http/Controllers/Teacher/Question/Vocabulary/FillInTheGap/FillInTheGapController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Teacher\Question\Vocabulary\FillInTheGap;
+
+use App\Http\Controllers\Controller;
+use App\Model\Vocabulary\FillInTheGap\FillInTheGap;
+use Illuminate\Http\Request;
+
+class FillInTheGapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Vocabulary\FillInTheGap\FillInTheGap  $fillInTheGap
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FillInTheGap $fillInTheGap)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Vocabulary\FillInTheGap\FillInTheGap  $fillInTheGap
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FillInTheGap $fillInTheGap)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Vocabulary\FillInTheGap\FillInTheGap  $fillInTheGap
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FillInTheGap $fillInTheGap)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Vocabulary\FillInTheGap\FillInTheGap  $fillInTheGap
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FillInTheGap $fillInTheGap)
+    {
+        //
+    }
+}
---
+++ app/Model/Vocabulary/FillInTheGap/FillInTheGap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Vocabulary\FillInTheGap;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FillInTheGap extends Model
+{
+    protected $guarded = [];
+}
