7547ddcef3c7c444efca75a7c98aae1578bffa0d
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-04-07 23:40:54 +0600

    Create HeadingOption Resource files

---
+++ app/Model/Reading/Heading/HeadingOption.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Reading\Heading;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HeadingOption extends Model
+{
+    protected $guarded = [];
+}
---
+++ database/factories/HeadingOptionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Reading\Heading\HeadingOption;
+use Faker\Generator as Faker;
+
+$factory->define(HeadingOption::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
