04a23d8a8de39991d9d57ee67d08f97fdb1435a3
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-04-13 01:53:14 +0600

    Install Twilio for text sms

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Student;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Twilio\Rest\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,15 @@ Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('test', function () {
-    $students = Student::where('location_id', Auth::guard('teacher')->user()->location->id)->get();
-    return $students;
+
+    $sid = 'AC2667917e1123a447e4dcd07498cf3f10';
+    $token = '4243baaf525f396166e2f6597a9c2573';
+
+    $client = new Client($sid, $token);
+    for ($n = 1; $n < 100; $n++) {
+        $client->messages->create('+8801740-915311', [
+            'from' => '+15866666750',
+            'body' => "Hi, Supto {$n}"
+        ]);
+    }
 });
