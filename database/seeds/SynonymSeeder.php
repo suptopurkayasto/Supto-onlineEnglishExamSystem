4820425ca33c099a2fb6c0a48cecdefdb2a4a8c2
Supto Purkayasto <suptopurkayasto@gmail.com>
2020-04-22 12:44:07 +0600

    Modify Teacher/Synonym

--- database/seeds/SynonymSeeder.php
+++ database/seeds/SynonymSeeder.php
@@ -11,11 +11,31 @@ class SynonymSeeder extends Seeder
      */
     public function run()
     {
-        for ($set = 1; $set <= 4; $set++) {
-            for ($number = 1; $number <= 5; $number++) {
-                factory(\App\Model\Vocabulary\Synonym\Synonym::class)->create(['set_id' => $set, 'synonym_option_id' => $number]);
-            }
-            factory(\App\Model\Vocabulary\Synonym\SynonymOption::class, 10)->create(['set_id' => $set]);
+        // Set A
+        for ($number = 1; $number <= 5; $number++) {
+            factory(\App\Model\Vocabulary\Synonym\Synonym::class)->create(['set_id' => 1, 'synonym_option_id' => $number]);
         }
+        factory(\App\Model\Vocabulary\Synonym\SynonymOption::class, 10)->create(['set_id' => 1]);
+
+
+        // Set B
+        for ($number = 11; $number <= 15; $number++) {
+            factory(\App\Model\Vocabulary\Synonym\Synonym::class)->create(['set_id' => 2, 'synonym_option_id' => $number]);
+        }
+        factory(\App\Model\Vocabulary\Synonym\SynonymOption::class, 10)->create(['set_id' => 2]);
+
+
+        // Set C
+        for ($number = 21; $number <= 25; $number++) {
+            factory(\App\Model\Vocabulary\Synonym\Synonym::class)->create(['set_id' => 3, 'synonym_option_id' => $number]);
+        }
+        factory(\App\Model\Vocabulary\Synonym\SynonymOption::class, 10)->create(['set_id' => 3]);
+
+
+        // Set D
+        for ($number = 31; $number <= 35; $number++) {
+            factory(\App\Model\Vocabulary\Synonym\Synonym::class)->create(['set_id' => 4, 'synonym_option_id' => $number]);
+        }
+        factory(\App\Model\Vocabulary\Synonym\SynonymOption::class, 10)->create(['set_id' => 4]);
     }
 }
--- resources/views/teacher/questions/vocabulary/synonym/index.blade.php
+++ resources/views/teacher/questions/vocabulary/synonym/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.teacher')
 
-@section('title', 'All Synonym Questions')
+@section('title', 'All Synonym Words')
 
 @section('content')
     @if($authTeacher->exams()->count() > 0)
@@ -9,9 +9,13 @@
                 <div class="card mb-5 index-card">
                     <div
                         class="card-header">
-                        <h3 class="card-title float-left index-card-title" title="{{ $exam->name }}"><span
-                                class="font-weight-bolder">{{ Str::limit($exam->name, 30) }}</span>
-                            Synonym Word
+                        <h3 class="card-title index-card-title float-left {{ $exam->synonyms()->count() === 20 && $exam->synonymOptions()->count() === 40 ? 'text-success' : 'text-warning' }}" title="{{ $exam->name }}">
+                            <span>{{ Str::limit($exam->name, 30) }}</span>
+                            <span class="font-weight-bolder ml-2">Synonym Words</span>
+                            @if($exam->synonyms()->count() === 20 && $exam->synonymOptions()->count() === 40)
+                                <i class="fas fa-check-circle"></i>
+                            @endif
+
                         </h3>
                         @if($exam->synonyms()->count() !== 20)
                             <a href="{{ route('teachers.questions.synonyms.create') }}?exam={{ encrypt($exam->id) }}"
@@ -21,43 +25,44 @@
                         @endif
                     </div><!-- /.card-header -->
                     @if($exam->synonyms()->count() === 20 && $exam->synonymOptions()->count() === 40)
-                        <div class="progress" style="height: 7px">
-                            <div class="progress-bar progress-bar-striped bg-primary progress-bar-animated" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     @else
-                        <div class="progress" style="height: 7px">
-                            <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                     @endif
                     <div class="card-body">
                         <div class="row">
                             @foreach($exam->sets as $set)
-                                @php $synonymCountBySet = $exam->synonyms()->where('set_id', $set->id)->get()->count() @endphp
+                                <?php
+                                $synonymCountBySet = $exam->synonyms()->where('set_id', $set->id)->get()->count();
+                                $synonymOptionsCountBySet = $exam->synonymOptions()->where('set_id', $set->id)->get()->count();
+                                ?>
                                 <div class="col-12 col-md-6 col-lg-3 count-section">
-                                    <div class="info-box bg-white border-primary border">
-                                    <span class="info-box-icon text-primary"
+                                    <div class="info-box bg-white border {{ $synonymCountBySet === 5 && $synonymOptionsCountBySet === 10? ' border-success': ' border-warning' }}">
+                                    <span class="info-box-icon text-white {{ $synonymCountBySet === 5 && $synonymOptionsCountBySet === 10? 'bg-success': 'bg-warning' }}"
                                           style="font-weight: 900">{{ $set->name }}</span>
                                         <div class="info-box-content">
                                             <span class="info-box-number font-weight-normal">{{ $synonymCountBySet }} / 5 Synonym word.</span>
 
                                             <div class="progress">
-                                                <div class="progress-bar"
+                                                <div class="progress-bar {{ $synonymCountBySet === 5 ? 'bg-success': 'bg-warning' }}"
                                                      style="width: {{ ($synonymCountBySet*100)/5 }}%"></div>
                                             </div>
                                             <div class="progress-description">
                                                 @if($synonymCountBySet === 5)
-                                                    @if($exam->synonymOptions()->where('set_id', $set->id)->get()->count() === 10)
+                                                    @if($synonymOptionsCountBySet === 10)
                                                         <a href="{{ route('teachers.questions.synonyms.options.index') }}?exam={{ encrypt($exam->id) }}&set={{ encrypt($set->id) }}"
-                                                           class="btn btn-sm btn-primary btn-block">View
-                                                            Option</a>
+                                                           class="btn-link text-success"><i class="fas fa-eye"></i> View option</a>
                                                     @else
                                                         <a href="{{ route('teachers.questions.synonyms.options.create') }}?exam={{ encrypt($exam->id) }}&set={{ encrypt($set->id) }}"
-                                                           class="btn btn-sm btn-outline-primary btn-block">Add
-                                                            Options</a>
+                                                           class="btn-link"><i class="fas fa-pen-square"></i> Add options</a>
                                                     @endif
                                                 @else
                                                     <a href="{{ route('teachers.questions.synonyms.create') }}?exam={{ encrypt($exam->id) }}&set={{ encrypt($set->id) }}"
-                                                       class="btn btn-sm btn-outline-primary btn-block">Add Words</a>
+                                                       class="btn-link"><i class="fas fa-pen-square"></i> Add synonym word</a>
                                                 @endif
                                             </div>
                                         </div>
@@ -68,25 +73,27 @@
 
                             <div class="col-12">
                                 <table
-                                    id="example"
-                                    class="table table-striped table-bordered dt-responsive nowrap border-0 table-hover custom-table-style"
+                                    id=""
+                                    class="example table table-striped table-bordered dt-responsive nowrap border-0 table-hover custom-table-style"
                                     style="width: 100%">
                                     <thead>
                                     <tr>
                                         <th>#</th>
                                         <th>Word</th>
+                                        <th>Answer</th>
                                         <th>Set</th>
                                         <th>Exam</th>
                                         <th>Action</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($exam->synonyms as $index => $synonym)
+                                    @foreach($exam->synonyms()->orderByDesc('id')->get() as $index => $synonym)
                                         <tr>
                                             <td>{{ $index + 1 }}</td>
-                                            <td title="{{ $synonym->word }}">{{ Str::limit($synonym->word, 70) }}</td>
+                                            <td title="{{ $synonym->word }}">{{ $synonym->word }}</td>
+                                            <td title="{{ $synonym->answer->options }}">{{ $synonym->answer->options }}</td>
                                             <td>{{ $synonym->set->name }}</td>
-                                            <td>{{ $synonym->exam->name }}</td>
+                                            <td title="{{ $synonym->exam->name }}">{{ Str::limit($synonym->exam->name, 50) }}</td>
                                             <td class="text-center">
                                                 <a href="{{ route('teachers.questions.synonyms.show', $synonym->id) }}?exam={{ encrypt($synonym->exam->id) }}&set={{ encrypt($synonym->set->id) }}"
                                                    class="btn btn-primary btn-sm btn-block btn-hover-effect"><i
